3674421daefb36bc63cc072d6a248e746626aa04
Nina <nina@crake-2.local>
2014-07-11 22:51:17 +0200

    Basic playlist - it haz pics

--- public/javascripts/facebook_group.js
+++ public/javascripts/facebook_group.js
@@ -4,6 +4,9 @@ function checkGroupPermissions(groupId) {
       function (response) {
         if (response && !response.error) {
           console.log(response);
+          $('#fullGroupName').text(response.name);
+          $('#groupDescription').text(response.description);
+          FB.api('/' + groupId + '/feed', loadFeed);
         } else { // Error retrieving
           groupInputError('That group doesn\'t exist - the name can be found in the url: www.facebook.com/group/<COPY_ME>'); 
 
@@ -13,6 +16,21 @@ function checkGroupPermissions(groupId) {
   FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
   });
+}
+
+function loadFeed(response) {
+  if (response && !response.error) {
+    console.log(response);
+    var playlist = $('#playlist');
+    $.each(response.data, function(i, val) {
+      var innerDiv = $('<div></div>').
+        append($('<img/>', { src: val.picture}));
+      playlist.append(innerDiv);
+    });
+  } else { // Error retrieving
+    groupInputError('Error loading feed :('); 
+
+  }
 }
 
 function performRetrieval(groupId) {
